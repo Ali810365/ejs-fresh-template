8e1e7a76b45b7656153ddb609451a6cfc90c71d2
Ali Mohamed Abdi <105326067+Ali810365@users.noreply.github.com>
2022-08-17 08:07:07 +0000

    updates

--- index.js
+++ index.js
@@ -4,11 +4,60 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
 app.get('/', (req, res) => {
   //res.send('Ola from Express!')
-  var title = 'Our Home Page';
-  res.render('pages/index', {'title':title});
+  var name = 'Adele'
+  var headingOne = 'Early life and Education'
+  var headingTwo = 'Career Debut'
+  var headingThree = 'Personal life'
+  var headingFour = 'Musical style'
+  
+  res.render('pages/index', {
+    'name':name,
+    'headingOne': headingOne,
+    'headingTwo': headingTwo,
+    'headingThree': headingThree,
+    'headingFour': headingFour
+  });
 })
+
+app.get('/wilson', (req, res) => {
+  //res.send('Ola from Express!')
+  var name = 'Russell'
+  var headingOne = 'Early life and Education'
+  var headingTwo = 'College Football Career'
+  var headingThree = 'NFL Career'
+  var headingFour = 'Charitable work'
+  res.render('pages/wilson', {
+    'name':name,
+    'headingOne': headingOne,
+    'headingTwo': headingTwo,
+    'headingThree': headingThree,
+    'headingFour': headingFour
+  });
+})
+
+app.get('/ciara', (req, res) => {
+  //res.send('Ola from Express!')
+  var name = 'Ciara'
+  var headingOne = 'Early life and Education'
+  var headingTwo = 'Personal Life'
+  var headingThree = 'Vocals and Musical Style'
+  var headingFour = 'Influences'
+  res.render('pages/ciara', {
+    'name':name,
+    'headingOne': headingOne,
+    'headingTwo': headingTwo,
+    'headingThree': headingThree,
+    'headingFour': headingFour
+  });
+})
+
+
 
 
 
